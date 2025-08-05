be375f0c8f528ea64d0ca7efed1669a5297a439c
orn-chfotm <chfotm@gmail.com>
2025-08-06 01:14:31 +0900

    강의 12강 공통 부분 구현

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,32 +1,44 @@
 import "./App.css";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { useReducer } from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
+import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
+
+const mockDate = [
+   {
+      id: 1,
+      createdDate: new Date().getTime(),
+      emotionId: 1,
+      content: "1번 일기 내용",
+   },
+   {
+      id: 2,
+      createdDate: new Date().getTime(),
+      emotionId: 2,
+      content: "2번 일기 내용",
+   },
+];
+
+function reducer(state, action) {
+   return state;
+}
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-   const nav = useNavigate();
-
-   const onClickButton = () => {
-      nav("/new");
-   };
+   const [data, dispatch] = useReducer(reducer, mockDate);
 
    return (
       <>
-         <div>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/new"}>New</Link>
-            <Link to={"/diary"}>Diary</Link>
-         </div>
-         <button onClick={onClickButton}>New 페이지로 이동</button>
          <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/new" element={<New />}></Route>
             <Route path="/diary/:id" element={<Diary />}></Route>
+            <Route path="/edit/:id" element={<Edit />}></Route>
             <Route path="*" element={<Notfound />}></Route>
          </Routes>
       </>
