9b8483170e13bf0c8ffc5df388dee9e4abaf6f6d
orn-chfotm <chfotm@gmail.com>
2025-08-09 02:34:13 +0900

    2025-08-09 Home UI 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -87,28 +87,6 @@ function App() {
 
    return (
       <>
-         <button
-            onClick={() => {
-               onCreate(new Date().getTime(), 1, "hello");
-            }}
-         >
-            일기 추가 테스트
-         </button>
-         <button
-            onClick={() => {
-               onUpdate(1, new Date().getTime(), 2, "hello");
-            }}
-         >
-            일기 수정 테스트
-         </button>
-         <button
-            onClick={() => {
-               onDelete(1);
-            }}
-         >
-            일기 삭제 테스트
-         </button>
-
          <DiaryStateContext.Provider value={data}>
             <DiaryDispatchContent.Provider
                value={{ onCreate, onUpdate, onDelete }}
